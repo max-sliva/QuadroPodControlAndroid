2a4bb4b2634dc28dde8bbc3e6f71db22c3db8a02
max-sliva <persimon@inbox.ru>
2024-02-07 17:09:18 +0500

    started second activity realization

--- app/src/main/java/com/example/quadropodcontrol/RobotMovingActivity.kt
+++ app/src/main/java/com/example/quadropodcontrol/RobotMovingActivity.kt
@@ -3,12 +3,21 @@ package com.example.quadropodcontrol
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import com.example.quadropodcontrol.ui.theme.QuadroPodControlTheme
 
 class RobotMovingActivity : ComponentActivity() {
@@ -21,7 +30,7 @@ class RobotMovingActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting2("Robot Moving")
+                    ControlBox("Robot Moving")
                 }
             }
         }
@@ -29,9 +38,91 @@ class RobotMovingActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ControlBox(name: String, modifier: Modifier = Modifier) {
+    var startPointX by remember { mutableStateOf(0f) }
+    var ratio by remember {   mutableStateOf(0f)     }
+    var startPointY by remember { mutableStateOf(0f) }
+    var offsetX by remember { mutableStateOf(0f) }
+    var offsetY by remember { mutableStateOf(0f) }
+
+    Canvas(modifier = Modifier
+        .fillMaxSize()
+        .pointerInput(Unit) {
+//                    detectTapGestures(
+//                        onLongPress = {
+//                            println("x = ${it.x}  y = ${it.y}")
+//                        }
+//                    )
+//                   if (degs<=65)
+            detectDragGestures(
+                onDragStart = { touch ->
+//                            println("\nStart of the interaction is x=${touch.x} y=${touch.y}")
+                    startPointX = touch.x
+                    startPointY = touch.y
+                    offsetX = 0F //сбрасываем оффсеты, чтобы нормально двигать ногу
+                    offsetY = 0F
+//                            val ratio = quadroPodBody.width / size.width
+                    println("ratio in dragStart = $ratio")
+
+//                    var number =
+//                        getArmNumber(startPointX, quadroPodBody, startPointY, ratio)
+//                    startPointXArray[number] = startPointX
+//                    startPointYArray[number] = startPointY
+//                    offsetXArray[number] = offsetX
+//                    offsetYArray[number] = offsetY
+                },
+                onDrag = { change, dragAmount ->
+                    change.consume()
+//                            println("in listener x    = ${dragAmount.x}  y = ${dragAmount.y}  ")
+//                            println("arm1RotatePointX = $arm1RotatePointX arm1RotatePointY = $arm1RotatePointY" )
+                    offsetX += dragAmount.x
+                    offsetY += dragAmount.y
+                    var number = 0
+//                            val ratio = quadroPodBody.width / size.width
+                    println("ratio in dragStart = $ratio")
+//                    number = getArmNumber(
+//                        startPointX,
+//                        quadroPodBody,
+//                        startPointY,
+//                        ratio
+//                    )    //для leg4
+//                    offsetXArray[number] += dragAmount.x
+//                    offsetYArray[number] += dragAmount.y
+                },
+                onDragEnd = {
+//                            println("angle on drag end = $degs")
+//                           angleOnDragEnd = degs
+//                            val ratio = quadroPodBody.width / size.width
+//                    val number = getArmNumber(
+//                        startPointX,
+//                        quadroPodBody,
+//                        startPointY,
+//                        ratio
+//                    )
+//                    if (curDeviceName != "") writeArmAngleToArduino(
+//                        bltWork,
+//                        socketToDevice!!,
+//                        number,
+//                        degsForArms[number]
+//                    )
+                },
+            )
+        }){
+        val canvasWidth = size.width
+        val circleRadius = canvasWidth / 8f
+        // TODO: сделать переменную для центра большого круга и определять пределы обработки касания
+        drawCircle( //то рисуем его
+            Color.Gray, //цвет рисования
+            radius = canvasWidth / 3f //и радиус
+        )
+        drawCircle( //то рисуем его
+            Color.Blue, //цвет рисования
+            radius = circleRadius, //и радиус
+            center = Offset(canvasWidth / 2+offsetX, size.height / 2 + offsetY)
+        )
+    }
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
 }
